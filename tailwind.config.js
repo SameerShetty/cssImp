fd3169ae5ae7298ccffe2db603185ae90ef7842d
SameerShetty <kalyanshettisameer@gmail.com>
2024-01-16 23:29:21 +0530

    css imp

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "576px",
+        mD: "835px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        xxl: "1400px",
+      },
+    },
+  },
+  plugins: [],
+};
